3d6949fe3334282aa48c6fc065c3f1114177dd85
+ <+>
2023-07-31 09:54:37 +0300

    Notelist update

--- src/components/noteslists/NoteList.js
+++ src/components/noteslists/NoteList.js
@@ -2,17 +2,20 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { NotesContext } from "../../utils/NotesContext";
 
-export default function NoteList () {
-    const { notes } = useContext(NotesContext);
+export default function NoteList() {
+  const { notes } = useContext(NotesContext);
 
-    return (
-        <ul>
-          {notes.map((note) => (
-            <li key={note.id}>
-                {/* creating a route to display a component with the notes details */}
-              <Link to={`/note/${note.id}`}>{note.content}</Link>
-            </li>
-          ))}
-        </ul>
-      );
+  return (
+    <div>
+      <h4>Note List</h4>
+      <ul>
+        {notes.map((note) => (
+          <li key={note.id}>
+            {/* creating a route to display a component with the notes details */}
+            <Link to={`/note/${note.id}`}>{note.content}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
